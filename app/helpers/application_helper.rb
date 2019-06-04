0d4cb682eca403b94539fbc7c27703cdb7fba0e7
Camilo Moreno <camilomoreno1488@live.com>
2019-06-04 11:19:25 -0500

    Add field and input difficulty on views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def activity_difficulty(activity)
+    if activity.difficulty == "low"
+      content_tag(:span, "#{activity.difficulty}", class: "badge badge-primary")
+    elsif activity.difficulty == "medium"
+      content_tag(:span, "#{activity.difficulty}", class: "badge badge-warning")
+    else
+      content_tag(:span, "#{activity.difficulty}", class: "badge badge-danger")
+    end
+  end
 end
