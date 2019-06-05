ec78311ed1e8ecacd4eb9d68a461d4a99a819a31
kmilo1488 <camilomoreno1488@live.com>
2019-06-04 22:52:47 -0500

    Search

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,12 @@
 class VenuesController < ApplicationController
   def index
-    @venues = Venue.all
+    @search = params["search"]
+    @venues = if @search.present?
+                @name = @search["name"]
+                Venue.where("name ILIKE ?", "%#{@name}%")
+              else
+                Venue.all
+              end
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -11,7 +11,5 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :activities
 
-  scope :by_name, -> { map{ |c| [c.name, c.id] } }
-
   validates :name, presence: true, uniqueness: true
 end
