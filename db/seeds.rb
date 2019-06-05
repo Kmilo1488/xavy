e86009e576ccd77b1177721f8d38aee11eb19127
kmilo1488 <camilomoreno1488@live.com>
2019-06-04 20:50:27 -0500

    Update Seeds
    Add Categories into other views

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,14 +37,63 @@ venues.each.with_index(1) do |venue, index|
   )
 end
 
-Activity.create(name: 'Clase de crossfit', description: 'Ven a entrenar', schedule: 'Lunes a viernes De 8:00am a 5:00pm', venue: Venue.first, difficulty: 0)
-Activity.create(name: 'Entrada libre', description: 'Ven a entrenar', schedule: 'Todos los díasa las 5:00pm', venue: Venue.first, difficulty: 2)
+Category.create(name: "Crossfit")
+Category.create(name: "Yoga")
+Category.create(name: "Escalada")
+Category.create(name: "Pilates")
 
-Activity.create(name: 'Kundalin yoga', description: 'Cuerpo y mente sana', schedule: 'De 8:00am a 5:00pm', venue: Venue.second, difficulty: 1)
-Activity.create(name: 'Yoga meditación', description: 'Paz mental', schedule: 'De 8:00am a 10:00m y de 2:00pm a 5pm', venue: Venue.second, difficulty: 2)
+Activity.create(name: 'Clase de crossfit',
+                description: 'Ven a entrenar',
+                schedule: 'Lunes a viernes De 8:00am a 5:00pm',
+                venue: Venue.first,
+                difficulty: 0,
+                category_ids: [1])
 
-Activity.create(name: 'Clase de escalada', description: 'Aprende las bases', schedule: 'De 8:00am a 5:00pm', venue: Venue.third, difficulty: 0)
-Activity.create(name: 'Entrada libre', description: 'Una hora de acceso a todos los salones', schedule: 'De 8:00am a 5:00pm', venue: Venue.third, difficulty: 1)
+Activity.create(name: 'Entrada libre',
+                description: 'Ven a entrenar',
+                schedule: 'Todos los díasa las 5:00pm',
+                venue: Venue.first,
+                difficulty: 2,
+                category_ids: [1, 2, 3, 4])
 
-Activity.create(name: 'Pilates reformer', description: 'Estírate', schedule: 'De 8:00am a 5:00pm', venue: Venue.fourth, difficulty: 2)
-Activity.create(name: 'Pilates re-reformer', description: 'Restírate', schedule: 'De 8:00am a 5:00pm', venue: Venue.fourth, difficulty: 1)
+Activity.create(name: 'Kundalin yoga',
+                description: 'Cuerpo y mente sana',
+                schedule: 'De 8:00am a 5:00pm',
+                venue: Venue.second,
+                difficulty: 1,
+                category_ids: [2])
+
+Activity.create(name: 'Yoga meditación',
+                description: 'Paz mental',
+                schedule: 'De 8:00am a 10:00m y de 2:00pm a 5pm',
+                venue: Venue.second,
+                difficulty: 2,
+                category_ids: [2])
+
+Activity.create(name: 'Clase de escalada',
+                description: 'Aprende las bases',
+                schedule: 'De 8:00am a 5:00pm',
+                venue: Venue.third,
+                difficulty: 0,
+                category_ids: [3])
+
+Activity.create(name: 'Entrada libre',
+                description: 'Una hora de acceso a todos los salones',
+                schedule: 'De 8:00am a 5:00pm',
+                venue: Venue.third,
+                difficulty: 1,
+                category_ids: [1, 2, 3, 4])
+
+Activity.create(name: 'Pilates reformer',
+                description: 'Estírate',
+                schedule: 'De 8:00am a 5:00pm',
+                venue: Venue.fourth,
+                difficulty: 2,
+                category_ids: [4])
+
+Activity.create(name: 'Pilates re-reformer',
+                description: 'Restírate',
+                schedule: 'De 8:00am a 5:00pm',
+                venue: Venue.fourth,
+                difficulty: 1,
+                category_ids: [4])
